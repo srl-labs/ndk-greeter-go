b32d0d8331de33dc05affc9b05a5d58b9bc6e5c0
Roman Dodin <dodin.roman@gmail.com>
2023-12-12 22:24:26 +0200

    use heredoc

--- debug/prepare-debug.sh
+++ debug/prepare-debug.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+# ensure sockatsock is not present
+rm -f /tmp/socatsock
+
 ## Relevant Environment Variables
 # DEBUG_PORT -> Port that the dlv debugger listens on (default = 7000)
 # ENTRY_ID -> The cpm-filter ipv4-filter entry number to use for the dlv debugger allow rule
@@ -16,16 +21,18 @@ ip netns exec srbase-mgmt socat -d -d -d -lf /tmp/mgmt-socat.log TCP6-LISTEN:${D
 ip netns exec srbase socat -d -d -d -lf /tmp/base-socat.log UNIX-LISTEN:/tmp/socatsock,reuseaddr,fork TCP:127.0.0.1:${DPORT} &
 
 
-# Add a CPM-Filter rule
+# Add a CPM-Filter rule to allow the dlv debugger to connect
 EID="${ENTRY_ID:-103}"
 echo "Deploying cpm-filter ipv(4,6) entry ${EID}"
-sr_cli -ec <<<$(echo "set / acl cpm-filter ipv4-filter entry ${EID} description \"dlv debugger\"
+sr_cli -ec <<EOF
+set / acl cpm-filter ipv4-filter entry ${EID} description "dlv debugger"
 set / acl cpm-filter ipv4-filter entry ${EID} match protocol tcp
 set / acl cpm-filter ipv4-filter entry ${EID} match destination-port value ${DPORT}
 set / acl cpm-filter ipv4-filter entry ${EID} action accept
-set / acl cpm-filter ipv6-filter entry ${EID} description \"dlv debugger\"
+set / acl cpm-filter ipv6-filter entry ${EID} description "dlv debugger"
 set / acl cpm-filter ipv6-filter entry ${EID} match next-header tcp
 set / acl cpm-filter ipv6-filter entry ${EID} match destination-port value ${DPORT}
-set / acl cpm-filter ipv6-filter entry ${EID} action accept")
+set / acl cpm-filter ipv6-filter entry ${EID} action accept
+EOF
 
 echo "Done"
